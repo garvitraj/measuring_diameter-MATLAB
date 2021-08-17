57e20b0d6a093d0ec2c8267953b60fa0b985eadf
Garvitraj Pandey <pandeygarvitraj@outlook.com>
2021-08-17 22:54:58 +0530

    Update calculation.m

--- Code/Noise-Remove-&-Calculation/calculation.m
+++ Code/Noise-Remove-&-Calculation/calculation.m
@@ -3,3 +3,21 @@ figure(3)
 b = imbinarize(blue);
 imshow(b);
 
+h = imdistline;
+%
+pause();
+%
+dist = getDistance(h);
+u = menu('Choose measuring unit','Pixels','Mili Meters','Centi Meters');
+if (u==1)
+    msg = sprintf('The length of the object is: %0.2f Pixels\n',dist);
+    show_result = msgbox(msg);
+elseif (u==2)
+    dist_mm = dist*0.2645;
+    msg= sprintf('The length of the object is: %0.2f Mili Meters\n',dist_mm);
+    show_result = msgbox(msg);
+else
+    dist_cm = (dist*0.2645)/100;
+    msg = sprintf('The length of the object is: %0.2f Centi Meters\n',dist_cm);
+    show_result = msgbox(msg);
+end
